bf4666608766ef88f4f959cde474cd5b8ae2a352
Waza Craft <wazacraftRFID@gmail.com>
2024-02-14 11:46:36 -0500

    updated gitignore

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cli-FSD',
+    version='0.45',
+    author='JG',
+    author_email='wazacraftRFID@gmail.com',
+    description='LLM-enabled companion utility for your terminal.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/wazacraft/cli-FSD',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            '@=cli_FSD.main:main', 
+        ],
+    },
+    install_requires=[
+        'Flask',
+        'flask-cors',
+        'python-dotenv',
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
