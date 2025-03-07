e770dc483430b5b1c98f8d71d0c1aa3dbca6050d
Johnathan Greenaway <wazacraftRFID@gmail.com>
2025-03-06 21:12:30 -0500

    added version notation and visuals

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cli-FSD',
-    version='1.7.97',
+    version='1.8.0',
     author='JG',
     author_email='wazacraftRFID@gmail.com',
     description='LLM-enabled companion utility for your terminal.',
